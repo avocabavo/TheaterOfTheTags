487f5d6f26f7dad4d0fbc897e62b16fa95504950
Violet <avocabavo@gmail.com>
2026-04-09 21:15:12 -0500

    get yjs working with vue

--- theater-of-the-mind/src/App.vue
+++ theater-of-the-mind/src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import NavigationBar from './components/NavigationBar.vue'
+import PowerTag from './components/PowerTag.vue'
 </script>
 
 <template>
   <NavigationBar />
+  <main>
+    <PowerTag id="hero" />
+    <PowerTag id="villain" />
+  </main>
 </template>

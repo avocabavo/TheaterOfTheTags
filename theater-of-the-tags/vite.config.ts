55a3ec688a7645ff5fc15534f28f9e19f4976a94
Violet <avocabavo@gmail.com>
2026-04-19 10:37:50 -0500

    add github deployment yaml

--- theater-of-the-tags/vite.config.ts
+++ theater-of-the-tags/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode })=> {
+  const useWebRTC = mode === 'webrtc'
+  return {
+    resolve: {
+      alias: {
+        './libyjs': path.resolve(
+          __dirname,
+          useWebRTC ? './src/lib/yjs-webrtc.ts' : './src/lib/yjs'
+        )
+      }
+    },
+    plugins: [vue()],
+    base: '/theater-of-the-tags/',
+  }
 })

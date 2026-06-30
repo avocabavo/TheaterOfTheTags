9d129245008ed9f0fbc56db871bca4cd03175e13
Violet <avocabavo@gmail.com>
2026-06-29 23:24:54 -0500

    tighten up the status tag styling

--- theater-of-the-tags/src/components/StatusTag.vue
+++ theater-of-the-tags/src/components/StatusTag.vue
@@ -152,6 +152,11 @@ defineExpose({toJson})
   font-size: larger;
 }
 
+.status-tag-name h2 {
+  margin-top: 0.3rem;
+  margin-bottom: 0.6rem;
+}
+
 .status-row {
   display: flex;
   flex-direction: row;

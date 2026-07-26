ce618498dcf9ccd00a8f4789a4e381c3a3a95f12
Owen Lange <avocabavo@gmail.com>
2026-07-25 23:06:34 -0500

    show room name in page title

--- theater-of-the-tags/src/main.ts
+++ theater-of-the-tags/src/main.ts
@@ -3,5 +3,10 @@ import './style.css'
 import './styles/ui.css'
 import App from './App.vue'
 
+const roomName = window.location.pathname
+document.title = roomName === '/'
+  ? 'Theater of the Tags'
+  : `Tags - ${decodeURIComponent(roomName.slice(1))}`
+
 createApp(App)
   .mount('#app')
